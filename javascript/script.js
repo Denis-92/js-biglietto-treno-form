0b24c914d909a52d12c80948b3cb62569d0a1f34
Denis-92 <d.mozenic@gmail.com>
2022-08-25 14:53:01 +0200

    sistemato input button

--- javascript/script.js
+++ javascript/script.js
@@ -7,44 +7,54 @@ console.log('JS - OK!');
 
 const prezzoAcquistoHTMLElement = document.getElementById('prezzoAcquisto');
 
-// input distanza
-const distanzaDaPercorrereHTMLElement = document.getElementById('distanzaDaPercorrere');
+const inputBigliettoHTMLElement = document.getElementById('inputBiglietto');
 
-let distanzaDaPercorrere = parseInt(distanzaDaPercorrereHTMLElement.value);
+inputBigliettoHTMLElement.addEventListener('click', function (event) {
 
-// input eta'
-const etaPasseggeroHTMLElement = document.getElementById('etaPasseggero');
+    console.log(event);
 
-let etaPasseggero = parseInt(etaPasseggeroHTMLElement.value);
-// fine raccolta input --
+    // input distanza
+    const distanzaDaPercorrereHTMLElement = document.getElementById('distanzaDaPercorrere');
 
-const prezzo = 0.21;
+    const distanzaDaPercorrere = parseInt(distanzaDaPercorrereHTMLElement.value);
+    console.log('mostra import distanza.. da html', distanzaDaPercorrere);
 
-let costoBiglietto = distanzaDaPercorrere * prezzo;
-let prezzoFinale = distanzaDaPercorrere * prezzo;
+    // input eta'
+    const etaPasseggeroHTMLElement = document.getElementById('etaPasseggero');
 
-if ((isNaN(distanzaDaPercorrere)) || (isNaN(etaPasseggero))) {
-    document.getElementById('prezzoAcquisto').innerHTML = `Dati inseriti non validi`;
-}
-else {
-    if (etaPasseggero < 18) {
-        const scontoMinori = 0.8;
-        //console.log('sconto minori', scontoMinori);
-        //console.log('costo biglietto', costoBiglietto);
-        prezzoFinale = costoBiglietto * scontoMinori;
+    const etaPasseggero = parseInt(etaPasseggeroHTMLElement.value);
+    console.log('mostra import età.. da html', etaPasseggero);
+    // fine raccolta input --
+
+    const prezzo = 0.21;
+
+    let costoBiglietto = distanzaDaPercorrere * prezzo;
+    let prezzoFinale = distanzaDaPercorrere * prezzo;
+
+    if ((isNaN(distanzaDaPercorrere)) || (isNaN(etaPasseggero))) {
+        document.getElementById('prezzoAcquisto').innerHTML = `Dati inseriti non validi`;
     }
-
     else {
-
-        if (etaPasseggero > 65) {
-            const scontoSenior = 0.6;
-            prezzoFinale = costoBiglietto * scontoSenior;
+        if (etaPasseggero < 18) {
+            const scontoMinori = 0.8;
+            //console.log('sconto minori', scontoMinori);
+            //console.log('costo biglietto', costoBiglietto);
+            prezzoFinale = costoBiglietto * scontoMinori;
         }
 
+        else {
+
+            if (etaPasseggero > 65) {
+                const scontoSenior = 0.6;
+                prezzoFinale = costoBiglietto * scontoSenior;
+            }
+
+        }
+
+        prezzoFinale = prezzoFinale.toFixed(2);
+        console.log('prezzo finale prima del output', prezzoFinale);
+
+        document.getElementById('prezzoAcquisto').innerHTML = `Il prezzo del tuo biglietto è ${prezzoFinale} €`;
     }
 
-    prezzoFinale = prezzoFinale.toFixed(2);
-    console.log('prezzo finale prima del output', prezzoFinale);
-
-    document.getElementById('prezzoAcquisto').innerHTML = `Il prezzo del tuo biglietto è ${prezzoFinale} €`;
-}
+})
